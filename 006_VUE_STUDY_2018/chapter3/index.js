82075514f7746bded8dfbfa9c8110b60c58e55f7
Tan <tan@azest.co.jp>
2018-11-20 13:59:21 +0900

    vue componet

--- 006_VUE_STUDY_2018/chapter3/index.js
+++ 006_VUE_STUDY_2018/chapter3/index.js
@@ -25,3 +25,57 @@ Vue.component('input-date-with-today',{
 var exampleVue = new Vue({
     el:'#example'
 });
+
+Vue.component("fruits-item-name", {
+    props:{
+        frutisitem:{
+            type:Object,
+            required:true
+        }
+    },
+    template:'<li>{{frutisitem.name}}</li>'
+});
+
+new Vue({
+    el:'#fruits-componet',
+    data:{
+        frutisItems:[
+            {name:'梨'},
+            {name:'ライチ'},
+        ]
+    }
+})
+
+var counterButton = Vue.extend({
+    template:'<span>{{counter}}個 <button v-on:click="addToCart">追加</button></span>',
+    data:function(){
+        return {
+            counter:0
+        }
+    },
+    methods:{
+        addToCart: function(){
+            this.counter += 1;
+            this.$emit('mycustemorevent');
+        }
+    }
+})
+
+new Vue({
+    el:'#frtuis-counter',
+    components:{
+        'counter-button':counterButton
+    },
+    data:{
+        total:0,
+        fruits:[
+            {name:'梨SX'},
+            {name:'ライチXX'},
+        ]
+    },
+    methods:{
+        fatherMethodOnClick:function(){
+            this.total += 1;
+        }
+    }
+})
