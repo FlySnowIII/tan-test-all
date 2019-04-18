c447b1184acacca7e5e91565f244174f810a28fb
Azest Tan <tan@azest.co.jp>
2019-04-18 16:24:37 +0900

    nuxtテスト

---
+++ 015_NUXTJS/myfirstnuxt/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li><nuxt-link to="/">home</nuxt-link></li>
+        <li><nuxt-link to="/about">about</nuxt-link></li>
+        <li><nuxt-link to="/">home</nuxt-link></li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
