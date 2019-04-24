246e8159b357c84c6aa06f548368b4c4eca43809
Azest Tan <tan@azest.co.jp>
2019-04-24 11:11:38 +0900

    練習

--- 015_NUXTJS/myfirstnuxt/components/Nav.vue
+++ 015_NUXTJS/myfirstnuxt/components/Nav.vue
@@ -2,12 +2,32 @@
     <ul>
         <li><nuxt-link to="/">home</nuxt-link></li>
         <li><nuxt-link to="/about">about</nuxt-link></li>
+        <li><nuxt-link to="/about">about</nuxt-link></li>
+        <li><nuxt-link to="/about">about</nuxt-link></li>
+        <li><nuxt-link to="/about">about</nuxt-link></li>
+        <li><nuxt-link to="/about">about</nuxt-link></li>
+        <li><nuxt-link to="/about">about</nuxt-link></li>
         <li><nuxt-link to="/">home</nuxt-link></li>
+        <li><nuxt-link to="/">home</nuxt-link></li>
+
+        <li @click="send()">Clickme</li>
     </ul>
+
 </template>
 
 <script>
 export default {
+    data () {
+        return {
+            phone:0,
+            code:0
+        }
+    },
+    methods: {
+        send(){
+            console.log(this.phone);
+        }
+    }
 
 }
 </script>
